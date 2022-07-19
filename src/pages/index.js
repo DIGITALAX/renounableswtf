fd394c1769b014078855dc25d1b3ef805b577c60
James Lin <wonyy1988@outlook.com>
2022-07-18 21:46:30 -0400

    fix: twitter link

--- src/pages/index.js
+++ src/pages/index.js
@@ -295,8 +295,10 @@ function Landing(props) {
       </section>
 
       <section className='flex flex-row justify-between w-full p-3 md:p-5'>
-        <a className='animButton cursor-pointer'>
-          <img className='w-6 h-6 md:w-12 md:h-12' src='/images/homepage/footer/twitter.png' href="https://twitter.com/renounables" target="_blank" rel="noreferrer"/>
+        <a className='animButton cursor-pointer' href="https://twitter.com/renounables" target="_blank" rel="noreferrer">
+          <img
+            className='w-6 h-6 md:w-12 md:h-12' src='/images/homepage/footer/twitter.png'
+          />
         </a>
         <img className='w-12 md:w-24 object-contain' src='/images/homepage/footer/red-nounce.png' />
         <img className='w-6 h-6 md:w-12 md:h-12' src='/images/homepage/footer/zero.png' />
